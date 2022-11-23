f7fce5c5f6c1074ed5544de7c55103abe750dad0
Okumbe-tech <evanceokumbe97@gmail.com>
2022-11-23 14:45:30 +0300

    solving javascript variable lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution in this file!
+let companyname ='Scuber';
+const companyName = 'Scuber'
+let mostProfitableNeighborhood;
+mostProfitableNeighborhood = 'Chelsea';
+let companyCeo;
+companyCeo = 'Susan Smith';
